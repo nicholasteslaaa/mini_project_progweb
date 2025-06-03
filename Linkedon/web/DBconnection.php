0f3e7143f9451ab10827e7a8b93c96a848de796d
christian nicholas <143601631+nicholasteslaaa@users.noreply.github.com>
2025-06-03 15:39:18 +0700

    Delete Linkedon directory

--- Linkedon/web/DBconnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "linkedon";
-$conn = new mysqli($servername,$username,$password,$dbname);
-?>
--- Linkedon/web/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["email"]);
-unset($_SESSION["tipeUser"]);
-setcookie("email","", time() -3660,"/");
-header("Location:login_page.php");
-?>
